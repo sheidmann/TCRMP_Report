568808243fc5a6fc1460c4da54d4e79f2251c622
Sarah Heidmann <sarah.heidmann@gmail.com>
2021-02-22 09:30:51 -0400

    plot biomass at all sites

--- scripts/AbunBioRichOverTime_2020.R
+++ scripts/AbunBioRichOverTime_2020.R
@@ -127,3 +127,92 @@ abun.mesophotic <- ggplot(data = filter(abunsum, REEF.COMPLEX == "Mesophotic"),
 abunplot <- plot_grid(abun.nearshore,abun.offshore,abun.mesophotic,ncol = 1,align = "v")
 save_plot("outputs/abundance_bytime_splitreef_2020.jpeg", abunplot, 
           base_aspect_ratio = 0.8, base_height = 11)
+
+##### Biomass #####
+# g per 100m2 (one transect)
+# Sum all biomass on each transect, and take the average and SEM for each site
+biosum <- dat %>%
+   group_by(Year, Site, REEF.COMPLEX, Transect) %>%
+   summarize(totbio = sum(SumBiomass), .groups = "drop_last") %>%
+   summarize(meanbio = mean(totbio),
+             n = length(Transect),
+             sd = sd(totbio), .groups = "drop") %>%
+   mutate(sem = sd / sqrt(n))
+biosum
+
+# Nearshore
+bio.nearshore <- ggplot(data = filter(biosum, REEF.COMPLEX == "Nearshore"),
+                        aes(x=Year,y=meanbio,group=Site))+
+   geom_line(size=1,color="black",aes(linetype=Site))+
+   geom_point(size=4.5,color="black",aes(shape=Site,fill=Site))+
+   geom_errorbar(aes(ymax=meanbio+sem, ymin=meanbio-sem), width=0.2) +
+   scale_linetype_manual(values = nearshore.linetype,
+                         labels=nearshore.sites, 
+                         name= "Nearshore")+
+   scale_fill_manual(values = nearshore.color,
+                     labels=nearshore.sites, 
+                     name="Nearshore")+
+   scale_shape_manual(values = nearshore.shape,
+                      labels=nearshore.sites,
+                      name="Nearshore")+
+   panel+
+   legend+
+   ylab(expression(bold(paste("kg/100",m^2))))+
+   scale_y_continuous(limits = c(0,175000), breaks = seq(0,175000,40000),
+                      labels = seq(0,175000,40000)/1000) +
+   theme(axis.title.y = element_text(size = 14,face="bold"),
+         axis.text.y = element_text(size=12,face="bold"),
+         axis.text.x = element_text(angle=60,size=12,vjust=0.6,face="bold"),
+         axis.title.x = element_blank())
+# Offshore
+bio.offshore <- ggplot(data = filter(biosum, REEF.COMPLEX == "Offshore"),
+                       aes(x=Year,y=meanbio,group=Site))+
+   geom_line(size=1,color="black",aes(linetype=Site))+
+   geom_point(size=4.5,color="black",aes(shape=Site,fill=Site))+
+   geom_errorbar(aes(ymax=meanbio+sem, ymin=meanbio-sem), width=0.2) +
+   scale_linetype_manual(values = offshore.linetype,
+                         labels=offshore.sites, 
+                         name= "Offshore")+
+   scale_fill_manual(values = offshore.color,
+                     labels=offshore.sites, 
+                     name="Offshore")+
+   scale_shape_manual(values = offshore.shape,
+                      labels=offshore.sites,
+                      name="Offshore")+
+   panel+
+   legend+
+   ylab(expression(bold(paste("kg/100",m^2))))+
+   scale_y_continuous(limits = c(0,175000), breaks = seq(0,175000,40000),
+                      labels = seq(0,175000,40000)/1000) +
+   theme(axis.title.y = element_text(size = 14,face="bold"),
+         axis.text.y = element_text(size=12,face="bold"),
+         axis.text.x = element_text(angle=60,size=12,vjust=0.6,face="bold"),
+         axis.title.x = element_blank())
+# Mesophotic
+bio.mesophotic <- ggplot(data = filter(biosum, REEF.COMPLEX == "Mesophotic"),
+                         aes(x=Year,y=meanbio,group=Site))+
+   geom_line(size=1,color="black",aes(linetype=Site))+
+   geom_point(size=4.5,color="black",aes(shape=Site,fill=Site))+
+   geom_errorbar(aes(ymax=meanbio+sem, ymin=meanbio-sem), width=0.2) +
+   scale_linetype_manual(values = mesophotic.linetype,
+                         labels=mesophotic.sites, 
+                         name= "Mesophotic")+
+   scale_fill_manual(values = mesophotic.color,
+                     labels=mesophotic.sites, 
+                     name="Mesophotic")+
+   scale_shape_manual(values = mesophotic.shape,
+                      labels=mesophotic.sites,
+                      name="Mesophotic")+
+   panel+
+   legend+
+   ylab(expression(bold(paste("kg/100",m^2))))+
+   scale_y_continuous(limits = c(0,175000), breaks = seq(0,175000,40000),
+                      labels = seq(0,175000,40000)/1000) +
+   theme(axis.title.y = element_text(size = 14,face="bold"),
+         axis.text.y = element_text(size=12,face="bold"),
+         axis.text.x = element_text(angle=60,size=12,vjust=0.6,face="bold"),
+         axis.title.x = element_blank())
+
+bioplot <- plot_grid(bio.nearshore,bio.offshore,bio.mesophotic,ncol = 1,align = "v")
+save_plot("outputs/biomass_bytime_splitreef_2020.jpeg", bioplot, 
+          base_aspect_ratio = 0.8, base_height = 11)
