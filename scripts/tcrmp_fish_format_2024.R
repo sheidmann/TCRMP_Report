15834dcb5380ad092fa3e74bb7424638f1aa9d42
Sarah Heidmann <sarah.heidmann@gmail.com>
2025-03-06 16:42:48 -0400

    finish up 2024 fish QAQC

--- scripts/tcrmp_fish_format_2024.R
+++ scripts/tcrmp_fish_format_2024.R
@@ -1,7 +1,7 @@
-# tcrmp_fish_format_2023.R
+# tcrmp_fish_format_2024.R
 # Sarah Heidmann
-# Created 24 Oct 2024
-# Last modified 19 Nov 2024
+# Created 6 Mar 2025
+# Last modified 6 Mar 2025
 
 # This script takes csvs exported from our online data entry.
 # QAQC has already been done manually on this data.
@@ -13,14 +13,14 @@ library(lubridate)
 library(openxlsx)
 
 ##### Import the data #####
-reportperiod <- "2023"
+reportperiod <- "2024"
 period <- "Annual" # Annual or PostHurr or WS
 
 # Fish transects
 fts_raw <- read_csv(paste0("data/",reportperiod,"/TCRMP_FishTransects_",reportperiod,"_qaqc.csv"),
                     col_types = cols(Proofed = col_logical(),
                                      `Site name` = col_character(),
-                                     `Date completed`=col_date(format = "%m/%d/%y"),
+                                     `Date completed`=col_date(format = "%m/%d/%Y"),
                                      Name = col_character(),
                                      `Oc cc`= col_character(),
                                      Notes = col_character(),
@@ -30,23 +30,27 @@ fts_raw <- read_csv(paste0("data/",reportperiod,"/TCRMP_FishTransects_",reportpe
 
 # Fish rovers
 frs_raw <- read_csv(paste0("data/",reportperiod,"/TCRMP_FishRovers_",reportperiod,"_qaqc.csv"),
-                    col_types = cols(`Date completed`=col_date(format = "%m/%d/%y")))
+                    col_types = cols(`Date completed`=col_date(format = "%m/%d/%Y")))
 # Diademas
 das_raw <- read_csv(paste0("data/",reportperiod,"/TCRMP_Diadema_",reportperiod,"_qaqc.csv"),
-                    col_types = cols(`Date completed`=col_date(format = "%m/%d/%y")))
+                    col_types = cols(`Date completed`=col_date(format = "%m/%d/%Y")))
 
 # Fish trophic data
 trophs <- read_csv("data/fish_species_uvimaster.csv") %>% 
    select(`Scientific name`=ScientificName, TrophicGroup=TrophicLevel)
 
 ##### Fish Transects #####
-# Location, SampleYear, SampleMonth, Period, Transect, ScientificName, CommonName, TrophicGroup, 0-5:91-100, SppTotal
+# Location, SampleDate, SampleYear, SampleMonth, Period, Transect, Observer, ScientificName, CommonName, TrophicGroup, 0-5:>150, SppTotal
 fts_exp <- fts_raw %>% 
    # Split the date
-   mutate(SampleYear = year(`Date completed`),
+   mutate(SampleYear = reportperiod,
           SampleMonth = month(`Date completed`),
       # Add Period column
-          Period = period) %>%
+          Period = period,
+      SampleDate = format(`Date completed`, "%m/%d/%y")) %>%
+   # Reformat observer name
+   separate(Name, c("last","first")) %>% 
+   mutate(Observer = str_to_title(paste(first,last,sep=" "))) %>% 
    # Trophic classification
    left_join(trophs, by="Scientific name") %>% 
    # Species Total
@@ -54,8 +58,9 @@ fts_exp <- fts_raw %>%
    # Replace sizebin NAs with 0
    mutate(across(starts_with("X"), ~replace_na(.,0))) %>% 
    # Pick only needed columns and rename
-   select(Location=`Site name`, SampleYear, SampleMonth, Period, Transect=Rep, 
-          ScientificName=`Scientific name`, CommonName=`Common name`, TrophicGroup,
+   select(Location=`Site name`, SampleDate, SampleYear, SampleMonth, Period,
+          Transect=Rep, Observer,
+          ScientificName=`Scientific name`,CommonName=`Common name`,TrophicGroup,
           starts_with("X"), SppTotal) %>% 
    # Change gt to >
    rename_with(.data=., .fn = ~ gsub("to","-",gsub("gt","\u003E",.)), 
@@ -64,26 +69,30 @@ fts_exp <- fts_raw %>%
    rename_with(.data=., .fn = ~ gsub("to","-",gsub("X","",.)), 
                .cols = starts_with("X"))
 ##### Fish Rovers #####
-# We don't have a master sheet for this one so let's make it similar to transects
-# Location, SampleYear, SampleMonth, Period, Transect, ScientificName, CommonName, TrophicGroup, AbundanceIndex
+# Location, SampleDate, SampleYear, SampleMonth, Period, Transect, Observer, ScientificName, CommonName, TrophicGroup, AbundanceIndex
 frs_exp <- frs_raw %>% 
    # Split the date
-   mutate(SampleYear = year(`Date completed`),
+   mutate(SampleYear = reportperiod,
           SampleMonth = month(`Date completed`),
    # Add Period column
-          Period = period) %>% 
+          Period = period,
+   SampleDate = format(`Date completed`, "%m/%d/%y")) %>% 
+   # Reformat observer name
+   separate(Name, c("last","first")) %>% 
+   mutate(Observer = str_to_title(paste(first,last,sep=" "))) %>% 
    # Trophic classification
    left_join(trophs, by="Scientific name") %>% 
    # Pick only needed columns and rename
-   select(Location=`Site name`, SampleYear, SampleMonth, Period, Transect=Rep, 
-          ScientificName=`Scientific name`, CommonName=`Common name`, TrophicGroup,
+   select(Location=`Site name`, SampleDate, SampleYear, SampleMonth, Period,
+          Transect=Rep, Observer,
+          ScientificName=`Scientific name`,CommonName=`Common name`,TrophicGroup,
           AbundanceIndex=`Abundance index`)
 
 ##### Diademas #####
 # Location, SampleDate, SampleYear, SampleMonth, Period, Recorder (First Last), Transect, TestSize, Notes
-das_exp <- das_raw %>% 
+das_form <- das_raw %>% 
    # Split the date
-   mutate(SampleYear = year(`Date completed`),
+   mutate(SampleYear = reportperiod,
           SampleMonth = month(`Date completed`),
    # Add period column
           Period = period,
@@ -94,15 +103,21 @@ das_exp <- das_raw %>%
    # Pick only needed columns and rename
    select(Location=`Site name`, SampleDate, SampleYear, SampleMonth, Period,
           Recorder, Transect=Rep, TestSize=`Test size cm`, Notes)
+# Remove zeros to export sizes
+das_size <- das_form %>% 
+   # Take out the zeros and NAs
+   filter(TestSize!=0)
 # That's the individual sizes, but we also want one with density summarized.
 # Location, SampleDate, SampleYear, SampleMonth, Period, Recorder (First Last), Transect, TransSize, NoDiad, DiadDens, Notes
-das_sum <- das_exp %>% 
+das_dens <- das_form %>% 
    # Add transect size
    mutate(TransSize=50) %>% 
    # Summarize by ID variables
    group_by(Location, SampleDate, SampleYear, SampleMonth,Period,Recorder,
             Transect,TransSize,Notes) %>% 
-   summarize(NoDiad = length(which(TestSize!=0)), .groups="drop") %>% 
+   # NA as well as any number but 0 all count to density
+   summarize(NoDiad = length(which(TestSize!=0 | is.na(TestSize))), 
+             .groups="drop") %>% 
    # Calculate density
    mutate(DiadDens=NoDiad*100/TransSize) %>% 
    # Reorder columns
@@ -112,5 +127,5 @@ das_sum <- das_exp %>%
 ##### Export formatted data #####
 write.xlsx(fts_exp, paste0("outputs/TCRMP_FishTransects_",reportperiod,".xlsx"), na="")
 write.xlsx(frs_exp, paste0("outputs/TCRMP_FishRovers_",reportperiod,".xlsx"), na="")
-write.xlsx(das_exp, paste0("outputs/TCRMP_DiademaSize_",reportperiod,".xlsx"), na="")
-write.xlsx(das_sum, paste0("outputs/TCRMP_DiademaDens_",reportperiod,".xlsx"),na="")
+write.xlsx(das_size, paste0("outputs/TCRMP_DiademaSize_",reportperiod,".xlsx"), na="")
+write.xlsx(das_dens, paste0("outputs/TCRMP_DiademaDens_",reportperiod,".xlsx"),na="")
